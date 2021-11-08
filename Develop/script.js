270ff2570202335a7c75002e5272d3858e571eea
hanandadras <hanan.aabid@gmail.com>
2021-11-07 17:21:04 -0800

    added getItem method and styled the doc in css

--- Develop/script.js
+++ Develop/script.js
@@ -8,35 +8,24 @@ $(document).ready(function() {
     var fieldArray =[];
 
     saveBtn.on("click", function (){
-        var value = $(this).siblings('description').val()
+        var value = $(this).siblings('textarea').val()
         var time = $(this).parent().attr('id')
         localStorage.setItem(time, value)
     });
  
-    var hour = moment().format("k");
+    var hour =moment().format('MMMM Do YYYY, h:mm:ss a');
     console.log ("hour"+ hour);
-    for (let index=9; index <16; index++) {
-        let savedItem =localStorage.getItem("hour-" + index);
+    
 
-        if (savedItem) {
-            
-            $(".hour-" + index).val(savedItem);
-        }
-        console.log(index);
+    $(".task-9").val(localStorage.getItem('9'))
+    $(".task-10").val(localStorage.getItem('10'))
+    $(".task-11").val(localStorage.getItem('11'))
+    $(".task-12").val(localStorage.getItem('12'))
+    $(".task-13").val(localStorage.getItem('13'))
+    $(".task-14").val(localStorage.getItem('14'))
+    $(".task-15").val(localStorage.getItem('15'))
+    $(".task-16").val(localStorage.getItem('16'))
+    $(".task-17").val(localStorage.getItem('17'))
 
-        if (hour === index) {
-            $(".hour-" + index).addClass("present"); 
-        }
-        if (hour < index) {
-            $(".hour-" + index).addClass("future"); 
-        }
-        if (hour > index) {
-            $(".hour-" + index).addClass("past"); 
-        }
-    }
 
 });
-
-
-
-
